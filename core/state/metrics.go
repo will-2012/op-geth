bac9777d66331f7b358401f8c0a1266ad1b9f360
2020xibao <117156346+will-2012@users.noreply.github.com>
2025-04-02 17:28:13 +0800

    perf: add intermediateRootTimer in 500millisecond op-geth

--- core/state/metrics.go
+++ core/state/metrics.go
@@ -34,4 +34,7 @@ var (
 	slotDeletionCount    = metrics.NewRegisteredMeter("state/delete/storage/slot", nil)
 	slotDeletionSize     = metrics.NewRegisteredMeter("state/delete/storage/size", nil)
 	slotDeletionSkip     = metrics.NewRegisteredGauge("state/delete/storage/skip", nil)
+
+	// perf assemble cost
+	intermediateRootTimer = metrics.NewRegisteredTimer("intermediate/root/time", nil)
 )
