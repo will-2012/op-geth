aecb805214e210d1cd93753a013e7d86c68d29d2
2020xibao <117156346+will-2012@users.noreply.github.com>
2025-06-06 14:27:48 +0800

    chore: try fix stateless bug

--- core/state/state_object.go
+++ core/state/state_object.go
@@ -295,6 +295,7 @@ func (s *stateObject) finalise(prefetch bool) {
 	for key, value := range s.dirtyStorage {
 		s.pendingStorage[key] = value
 		if value != s.originStorage[key] {
+			s.GetCommittedState(key)
 			slotsToPrefetch = append(slotsToPrefetch, key) // Copy needed for closure
 		}
 	}
@@ -352,7 +353,7 @@ func (s *stateObject) finaliseRWSet() {
 // storage change at all.
 func (s *stateObject) updateTrie() (Trie, error) {
 	// Make sure all dirty slots are finalized into the pending storage area
-	// s.finalise(false)
+	s.finalise(false)
 
 	// Short circuit if nothing changed, don't bother with hashing anything
 	if len(s.pendingStorage) == 0 {
